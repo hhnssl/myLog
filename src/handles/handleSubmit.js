51443f4aad4448a50ca49a7979dd6d3ef017f1ae
hhnssl <losnah5h@gmail.com>
2023-01-26 15:32:14 +0900

    Feat - WritePage 글 업로드 기능

--- src/handles/handleSubmit.js
+++ src/handles/handleSubmit.js
@@ -1,15 +1,29 @@
 import { collection, addDoc } from 'firebase/firestore';
-import { firestoreDB } from '../firebase_setup/firebase';
+import { auth, firestoreDB } from '../firebase_setup/firebase';
 
 // collection과 document 만들기
-const handleSubmit = async (post, image) => {
+const handleSubmit = async (post, image, navigate) => {
+  console.log('auth.currentUser.displayName', auth.currentUser.id);
+
+  if ((post.postTitle === '') | (post.postContent === '')) {
+    alert('내용을 입력하세요!');
+    return false;
+  }
+
   try {
     const docRef = await addDoc(collection(firestoreDB, 'posts'), {
       postTitle: post.postTitle,
       image: image.name,
       postContent: post.postContent,
+      author: {
+        name: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
     });
     console.log('Document written with ID: ', docRef.id);
+
+    // 글 오리고 메인으로
+    navigate('/');
   } catch (e) {
     console.error('Error adding document: ', e);
   }
--- src/pages/WritePage/WritePage.jsx
+++ src/pages/WritePage/WritePage.jsx
@@ -1,15 +1,19 @@
 // 글을 작성하고 파이어베이스에 글을 전송하는 페이지
+import { auth, firestoreDB } from '../../firebase_setup/firebase';
+import { collection } from 'firebase/firestore';
 import { useState, useRef } from 'react';
 import BasicTemplate from '../../template/BasicTemplate';
-
 import handleSubmit from '../../handles/handleSubmit';
 // import handleImageUpload from '../../handles/handleFileUpload';
+import { useNavigate } from 'react-router-dom';
 
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../firebase_setup/firebase';
 
 const WritePage = ({ isAuth, handleSignOutClick }) => {
-  const postInputRef = useRef([]);
+  const navigate = useNavigate();
+
+  // const postInputRef = useRef([]);
   const [file, setFile] = useState();
   const [postInputState, setPostInputState] = useState({
     postTitle: '',
@@ -55,9 +59,9 @@ const WritePage = ({ isAuth, handleSignOutClick }) => {
     e.preventDefault();
     uploadToFirebase();
 
-    handleSubmit(postInputState, file);
+    handleSubmit(postInputState, file, navigate);
 
-    postInputRef.current.forEach((e) => (e.value = ''));
+    // postInputRef.current.forEach((e) => (e.value = ''));
   };
 
   return (
@@ -72,7 +76,7 @@ const WritePage = ({ isAuth, handleSignOutClick }) => {
             className="border-solid border-2 border-indigo-600"
             id="postTitle"
             name="postTitle"
-            ref={(elem) => (postInputRef.current[0] = elem)}
+            // ref={(elem) => (postInputRef.current[0] = elem)}
             value={postTitle}
             onChange={onChange}
             type="text"
@@ -86,7 +90,7 @@ const WritePage = ({ isAuth, handleSignOutClick }) => {
             className="border-solid border-2 border-indigo-600"
             type="file"
             accept="/image/*"
-            ref={(elem) => (postInputRef.current[1] = elem)}
+            // ref={(elem) => (postInputRef.current[1] = elem)}
             // value={postImage}
             onChange={handleChangeFile}
           />
@@ -98,7 +102,7 @@ const WritePage = ({ isAuth, handleSignOutClick }) => {
             name="postContent"
             id="postContent"
             placeholder="당신의 이야기를 적어보세요..."
-            ref={(elem) => (postInputRef.current[2] = elem)}
+            // ref={(elem) => (postInputRef.current[2] = elem)}
             value={postContent}
             onChange={onChange}
           ></textarea>
