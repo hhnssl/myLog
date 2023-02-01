8eb8eec5e75aa22bdfb9487c14b36355216f5183
hhnssl <losnah5h@gmail.com>
2023-02-01 13:32:48 +0900

    Fix - ViewPage-Post 반응형 디자인 적용

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,11 @@
 const Post = ({ post }) => {
   return (
-    <article className="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-2xl">
-      <h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:mb-6 lg:text-4xl">
+    <article className="p-4">
+      <h2 className="text-xl mb-6 font-extrabold leading-tight text-gray-900 sm:text-2xl">
         {post.postTitle}
       </h2>
-      <img className="" src={post.image} alt="" />
-      <p className=""> {post.postContent}</p>
+      <img className="mb-6 w-2/3 mx-auto sm:w-1/3" src={post.image} alt="" />
+      <p className="text-md leading-relaxed sm:text-lg"> {post.postContent}</p>
     </article>
   );
 };
