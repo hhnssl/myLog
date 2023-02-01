66b3e42947d3c7e1e180775f373021907d08f78b
hhnssl <losnah5h@gmail.com>
2023-02-01 10:44:58 +0900

    Fix - Post 컴포넌트 img 태그 src 수정

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,7 +4,7 @@ const Post = ({ post }) => {
       <h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:mb-6 lg:text-4xl">
         {post.postTitle}
       </h2>
-      <img className="" src="" alt="" />
+      <img className="" src={post.image} alt="" />
       <p className=""> {post.postContent}</p>
     </article>
   );
